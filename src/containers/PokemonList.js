fd0536a260769e0c61fb9389530f31e12134354c
Vince Garcia <vinceryangarcia@gmail.com>
2021-03-06 10:33:19 -0800

    test

---
+++ src/containers/PokemonList.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import _ from "lodash";
+import { GetPokemonList } from "../actions/pokemonActions";
+import {Link} from "react-router-dom";
+
+const PokemonList = () => {
+  const dispatch = useDispatch();
+  const pokemonList = useSelector(state => state.PokemonList);
+  React.useEffect(() => {
+    FetchData(1)
+  },[]);
+
+  const FetchData = (page) => {
+      dispatch(GetPokemonList (page))
+  }
+
+  const ShowData = () => {
+      if (!_.isEmpty(pokemonList.data)) {
+          return(
+              <div className={"list-wrapper"}>
+               {pokemonList.data.map( el => {
+            return(
+                <div className={"pokemon-item"}> 
+                <p>{el.name}</p>
+                <Link to ={`/pokemon/${el.name}`}>View</Link>
+            </div>
+               )
+          })}
+          </div>
+        )
+    }
+      if (pokemonList.loading) {
+            return <p>Loading...</p>
+    }
+      if (pokemonList.errorMsg !== "") {
+          return <p>{pokemonList.errorMsg}</p>
+      }
+     return <p>unable to get data</p>
+      };
+     return(
+    <div>
+        {ShowData()}
+    </div>
+  )
+};
+export default PokemonList
